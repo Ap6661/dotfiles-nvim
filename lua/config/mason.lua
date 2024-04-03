5d4d4eede937c3d1dde4ba55f0fa486cfeae81c1
APnda <59624475+Ap6661@users.noreply.github.com>
2024-04-03 15:53:33 -0500

    Fix Mason

--- lua/config/mason.lua
+++ lua/config/mason.lua
@@ -1,7 +1,6 @@
 return {
     'williamboman/mason.nvim',
     enabled = vim.fn.executable('nix-shell') ~= 1, -- Only enable if no Nix
-    branch = 'v2.x',
     dependencies = {
       { 'williamboman/mason-lspconfig.nvim' }, -- Optional
     },
