cd05d458aeca2e3fb1c7abfb6046d4a358e3f197
APnda <59624475+Ap6661@users.noreply.github.com>
2024-04-03 15:53:27 -0500

    lspsaga

--- lua/config/lsp-zero.lua
+++ lua/config/lsp-zero.lua
@@ -9,14 +9,33 @@ return {
       { 'hrsh7th/nvim-cmp' },     -- Required
       { 'hrsh7th/cmp-nvim-lsp' }, -- Required
       { 'L3MON4D3/LuaSnip' },     -- Required
+      -- LSP Saga
+      { 'nvimdev/lspsaga.nvim',
+        'nvim-treesitter/nvim-treesitter', -- optional
+        'nvim-tree/nvim-web-devicons',     -- optional
+      },
     },
     config = function()
       local lsp = require('lsp-zero').preset({})
+      local saga = require("lspsaga")
+      saga.setup()
 
+---@diagnostic disable-next-line: unused-local
       lsp.on_attach(function(client, bufnr)
         -- see :help lsp-zero-keybindings
         -- to learn the available actions
         lsp.default_keymaps({ buffer = bufnr })
+        vim.keymap.set('n', 'K', '<cmd>Lspsaga hover_doc<cr>', {buffer = bufnr})
+        vim.keymap.set('n', 'gd', '<cmd>Lspsaga peek_definition<cr>', {buffer = bufnr})
+        vim.keymap.set('n', 'go', '<cmd>Lspsaga peek_type_definition<cr>', {buffer = bufnr})
+        vim.keymap.set('n', '<F2>', '<cmd>Lspsaga rename<cr>', {buffer = bufnr})
+        vim.keymap.set('n', '<F4>', '<cmd>Lspsaga code_action<cr>', {buffer = bufnr})
+        vim.keymap.set('n', 'gl', '<cmd>Lspsaga show_line_diagnostics<cr>', {buffer = bufnr})
+        vim.keymap.set('n', '[d', '<cmd>Lspsaga diagnostic_jump_prev<cr>', {buffer = bufnr})
+        vim.keymap.set('n', ']d', '<cmd>Lspsaga diagnostic_jump_next<cr>', {buffer = bufnr})
+        vim.keymap.set('n', '[D', '<cmd>Lspsaga show_buf_diagnostics<cr>', {buffer = bufnr})
+        vim.keymap.set('n', ']D', '<cmd>Lspsaga show_workspace_diagnostics<cr>', {buffer = bufnr})
+
       end)
 
       lsp.set_sign_icons({
