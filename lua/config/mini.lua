d296bf57d1f3c37aab5d8d36b48bcfa98767d8ad
APnda <apnda6661@proton.me>
2024-04-23 13:10:56 -0500

    Rounded Corners and Comment Labels

--- lua/config/mini.lua
+++ lua/config/mini.lua
@@ -6,27 +6,43 @@ return {
       noremap = true,
       silent = true,
     }
+    -- Highlight Indent Block
     require("mini.indentscope").setup()
 
+    -- A little MiniMap to over view code with <leader>m
     require("mini.map").setup()
     vim.keymap.set('n', '<Leader>m', ":lua MiniMap.toggle()<CR>", opts)
 
+    -- Tab line with buffers
     require("mini.tabline").setup()
 
-    require("mini.comment").setup()
-
+    -- Notifications 
     require("mini.notify").setup()
 
-    require("mini.trailspace").setup()
+    -- Toggle a line as comments with gcc
+    require("mini.comment").setup()
 
+    -- Jump anywhere with <CR>
     require("mini.jump2d").setup()
 
-    require("mini.pick").setup()
+    -- Picker ( help, buffer, grep, etc )
+    require("mini.extra").setup()
+    require("mini.pick").setup({ window = { config = { border = 'rounded' } } })
     vim.keymap.set('n', '<Leader>ff', ":Pick ", {noremap = true})
 
+    -- File Explorer
     require("mini.files").setup()
     vim.keymap.set('n', '<Leader>-', function() MiniFiles.open() end, opts)
 
+    vim.api.nvim_create_autocmd('User', {
+      pattern = 'MiniFilesWindowOpen',
+      callback = function(args)
+        local win_id = args.data.win_id
+        vim.api.nvim_win_set_config(win_id, { border = 'rounded' })
+      end,
+    })
+
+    -- Underline Cursor Word, Toggle with <leader>cw
     require("mini.cursorword").setup()
     vim.keymap.set('n', '<Leader>cw', function()
       vim.g.minicursorword_disable = vim.g.minicursorword_disable == false
