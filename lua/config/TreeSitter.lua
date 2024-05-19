a019394ffd633033b8ba17f260b54d2e1d732f04
APnda <apnda6661@proton.me>
2024-05-19 00:06:09 -0500

    Treesitter defaults

--- lua/config/TreeSitter.lua
+++ lua/config/TreeSitter.lua
@@ -3,7 +3,7 @@ return {
   build = ":TSUpdate",
   config = function()
     require('nvim-treesitter.configs').setup {
-      ensure_installed = { "lua", "vim", "html", "javascript", "java" },
+      ensure_installed = { "lua", "vim", "html", "markdown", "markdown_inline", "regex", "javascript", "java" },
       sync_install = false,
       auto_install = true,
       highlight = {
