1f5a08da72380a9c0d348d44523890a443afeda3
APnda <apnda6661@proton.me>
2024-05-30 20:51:35 -0500

    Make harpoon silent

--- lua/config/harpoon.lua
+++ lua/config/harpoon.lua
@@ -4,12 +4,16 @@ return {
     {"nvim-lua/plenary.nvim"},
   },
   config = function ()
-    vim.keymap.set('n', '<Leader>[',      ':lua require("harpoon.ui").toggle_quick_menu()<Cr>')
-    vim.keymap.set('n', '<Leader><Tab>',  ':lua require("harpoon.mark").add_file()<Cr>')
-    vim.keymap.set('n', '<C-v>',          ':lua require("harpoon.ui").nav_next()<cr>')
-    vim.keymap.set('n', '<C-c>',          ':lua require("harpoon.ui").nav_prev()<cr>')
-    vim.keymap.set('n', '<leader>1',      ':lua require("harpoon.ui").nav_file(1)<cr>')
-    vim.keymap.set('n', '<leader>2',      ':lua require("harpoon.ui").nav_file(2)<cr>')
-    vim.keymap.set('n', '<leader>3',      ':lua require("harpoon.ui").nav_file(3)<cr>')
+    local opts = {
+      noremap = true,
+      silent = true,
+    }
+    vim.keymap.set('n', '<Leader>[',      ':lua require("harpoon.ui").toggle_quick_menu()<Cr>', opts)
+    vim.keymap.set('n', '<Leader><Tab>',  ':lua require("harpoon.mark").add_file()<Cr>', opts)
+    vim.keymap.set('n', '<C-v>',          ':lua require("harpoon.ui").nav_next()<cr>', opts)
+    vim.keymap.set('n', '<C-c>',          ':lua require("harpoon.ui").nav_prev()<cr>', opts)
+    vim.keymap.set('n', '<leader>1',      ':lua require("harpoon.ui").nav_file(1)<cr>', opts)
+    vim.keymap.set('n', '<leader>2',      ':lua require("harpoon.ui").nav_file(2)<cr>', opts)
+    vim.keymap.set('n', '<leader>3',      ':lua require("harpoon.ui").nav_file(3)<cr>', opts)
   end
 }
