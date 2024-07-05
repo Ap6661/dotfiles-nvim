1d5377c035f74b6294cee3939e32cd18d240fd6e
APnda <apnda6661@proton.me>
2024-07-05 15:53:49 -0500

    add nvim pager and env variables

--- nvim.nix
+++ nvim.nix
@@ -1,15 +1,26 @@
 { pkgs, home-manager, ...}:
 {
-  environment.systemPackages = with pkgs; [
-    neovim
-      lazygit
-      wget
-      git
-      gh
-      gcc
-      ripgrep
-      fd
-  ];
+  environment = {
+    systemPackages = with pkgs; [
+      neovim
+        lazygit
+        wget
+        git
+        gh
+        gcc
+        ripgrep
+        fd
+
+        nvimpager
+    ];
+
+    variables = { 
+      EDITOR = "nvim"; 
+      VISUAL = "nvim"; 
+      PAGER = "nvimpager";
+    };
+  };
+
   home-manager.users.apnda.home.file.".config/nvim" = {
     source = ./.;
     recursive = true;
