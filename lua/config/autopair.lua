97399bfb5319c27a5517df01d02416385722169a
APnda <apnda6661@proton.me>
2024-10-23 14:36:48 -0500

    Add toggle to Autopair (<leader><F3>)

--- lua/config/autopair.lua
+++ lua/config/autopair.lua
@@ -1,10 +1,12 @@
 return {
-  {
     'altermo/ultimate-autopair.nvim',
     event={'InsertEnter','CmdlineEnter'},
     branch='v0.6',
     opts={
       --Config goes here
     },
-  }
+    config = function ()
+      require'ultimate-autopair'.setup{}
+      vim.keymap.set('n', '<leader><F3>', ":lua require'ultimate-autopair'.toggle()<CR>")
+    end
 }
